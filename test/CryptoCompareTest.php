2d71e8f75b93a30b66e1c571919d30eebc2ffecf
Kailash Chandrasekaran <mail@ckailash.com>
2018-05-02 21:03:11 +1000

    Add tests

--- test/CryptoCompareTest.php
+++ test/CryptoCompareTest.php
@@ -2,8 +2,9 @@
 
 use PHPUnit\Framework\TestCase;
 use CoinTokenHub\CryptoCompareApi\CryptoCompare;
-use CoinTokenHub\CryptoCompareApi\Exception\CryptoCompareAuthException;
 use CoinTokenHub\CryptoCompareApi\Exception\CryptoCompareException;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
@@ -14,7 +15,7 @@ use GuzzleHttp\Psr7\Response;
  */
 class CryptoCompareTest extends TestCase {
 
-	public function createHttpClient($statusCode, $body)
+	private function createHttpClientAndQueueResponse($statusCode, $body)
 	{
 		$mock = new MockHandler(
 			array(
@@ -25,15 +26,64 @@ class CryptoCompareTest extends TestCase {
 		return new Client(array('handler' => $handler));
 	}
 
-	public function testRequestReturnsResponse() {
+	private function createHttpClientAndThrowException()
+	{
+		$mock = new MockHandler(
+			array(
+				new RequestException(
+					"Error communicating with the server",
+					new Request('GET', 'test')
+				)
+			)
+		);
+		$handler = HandlerStack::create($mock);
+		return new Client(array('handler' => $handler));
+	}
+
+	public function testRequestReturnsResponse()
+	{
 		$mockResponse = file_get_contents( __DIR__ . '/Mock/CryptoCompareTest/rate-limits-api-hours.txt' );
-		$ccApi = new CryptoCompare($this->createHttpClient(200, $mockResponse));
+		$ccApi = new CryptoCompare($this->createHttpClientAndQueueResponse(200, $mockResponse));
+		$this->assertEquals(
+			json_decode($mockResponse),
+			$ccApi->request("stats/rate/hour/limit")
+		);
+	}
+
+
+	public function testRequestThrowsException()
+	{
+		$ccApi = new CryptoCompare($this->createHttpClientAndThrowException());
+		$this->expectException(CryptoCompareException::class);
+		$ccApi->request("stats/rate/hour/limit");
+	}
+
+
+	public function testItReturnsRateLimitsForHours()
+	{
+		$mockResponse = file_get_contents( __DIR__ . '/Mock/CryptoCompareTest/rate-limits-api-hours.txt' );
+		$ccApi = new CryptoCompare($this->createHttpClientAndQueueResponse(200, $mockResponse));
 		$this->assertEquals(
 			json_decode($mockResponse),
 			$ccApi->rateLimits("hours")
 		);
 	}
 
+	public function testItReturnsRateLimitsForSeconds()
+	{
+		$mockResponse = file_get_contents( __DIR__ . '/Mock/CryptoCompareTest/rate-limits-api-seconds.txt' );
+		$ccApi = new CryptoCompare($this->createHttpClientAndQueueResponse(200, $mockResponse));
+		$this->assertEquals(
+			json_decode($mockResponse),
+			$ccApi->rateLimits("seconds")
+		);
+	}
 
+	public function testItThrowsExceptionForInvalidRateLimitTimeParameter()
+	{
+		$ccApi = new CryptoCompare($this->createHttpClientAndQueueResponse(200, ""));
+		$this->expectException(CryptoCompareException::class);
+		$ccApi->rateLimits("INVALID");
+	}
 
 }
